63608e56ed0330ac587980f6a03823450448a834
w.alexander <will_alexander@me.com>
2026-03-21 23:58:37 +0100

    Add unhappy path for product add

--- catalog/src/test/java/tech/reactiv/ecommerce/catalog/addtocatalog/AddToCatalogTest.java
+++ catalog/src/test/java/tech/reactiv/ecommerce/catalog/addtocatalog/AddToCatalogTest.java
@@ -5,13 +5,15 @@ import tech.reactiv.ecommerce.catalog.infrastructure.persistence.ProductSnapshot
 import tech.reactiv.ecommerce.catalog.product.ProductId;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AddToCatalogTest {
+
+    InMemoryProductRepository repository = new InMemoryProductRepository();
+    AddToCatalogHandler handler = new AddToCatalogHandler(repository);
+
     @Test
     void shouldAddProductToCatalog() {
-        var repository = new InMemoryProductRepository();
-        var handler = new AddToCatalogHandler(repository);
-
         var command = new AddToCatalogCommand("Product A", "Description A", 100);
         ProductId id = handler.handle(command);
 
@@ -20,5 +22,12 @@ public class AddToCatalogTest {
         assertThat(product.name()).isEqualTo("Product A");
         assertThat(product.description()).isEqualTo("Description A");
         assertThat(product.priceInCents()).isEqualTo(100);
+    }
+
+    @Test
+    void shouldRefuseInvalidProduct() {
+        var command = new AddToCatalogCommand("Product A", "Description A", -100);
+        assertThatThrownBy(() -> handler.handle(command))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 }
