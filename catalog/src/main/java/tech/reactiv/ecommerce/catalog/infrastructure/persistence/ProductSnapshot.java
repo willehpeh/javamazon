cc4e9c12f2644e89a5c6c6427aad0971b305f6ab
w.alexander <will_alexander@me.com>
2026-03-22 23:19:16 +0100

    Remove unnecessary ProductSnapshot record and its package

--- catalog/src/main/java/tech/reactiv/ecommerce/catalog/infrastructure/persistence/ProductSnapshot.java
+++
@@ -1,6 +0,0 @@
-package tech.reactiv.ecommerce.catalog.infrastructure.persistence;
-
-import java.util.UUID;
-
-public record ProductSnapshot(UUID id, String name, String description, int priceInCents, String category) {
-}
