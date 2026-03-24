c1f88fff5418b4455f74d401c91bc4c90427ec29
w.alexander <will_alexander@me.com>
2026-03-24 22:48:04 +0100

    Add empty CatalogController

---
+++ catalog/src/main/java/tech/reactiv/ecommerce/catalog/api/CatalogController.java
@@ -0,0 +1,9 @@
+package tech.reactiv.ecommerce.catalog.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/catalog")
+public class CatalogController {
+}
