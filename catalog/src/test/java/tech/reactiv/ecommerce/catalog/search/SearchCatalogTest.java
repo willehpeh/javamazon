c9fd5b5ccfc238ab5fced79eafc498111db99847
w.alexander <will_alexander@me.com>
2026-03-24 16:02:00 +0100

    Create parametrized test for non-matching search requests

--- catalog/src/test/java/tech/reactiv/ecommerce/catalog/search/SearchCatalogTest.java
+++ catalog/src/test/java/tech/reactiv/ecommerce/catalog/search/SearchCatalogTest.java
@@ -2,12 +2,15 @@ package tech.reactiv.ecommerce.catalog.search;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import tech.reactiv.ecommerce.catalog.product.InMemoryProductViews;
 import tech.reactiv.ecommerce.catalog.product.ProductId;
 import tech.reactiv.ecommerce.catalog.product.ProductView;
 import tech.reactiv.ecommerce.catalog.product.TestProductView;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -40,5 +43,19 @@ public class SearchCatalogTest {
 
         var foundProducts = handler.handle(request);
         assertThat(foundProducts).containsExactlyInAnyOrder(firstProduct, newProductWithSameCategory);
+    }
+
+    @ParameterizedTest
+    @MethodSource("nonMatchingRequests")
+    void shouldFindNoProductsIfNoneMatchRequest(SearchCatalogRequest request) {
+        var foundProducts = handler.handle(request);
+        assertThat(foundProducts).isEmpty();
+    }
+
+    static Stream<SearchCatalogRequest> nonMatchingRequests() {
+        return Stream.of(
+                new SearchCatalogRequest().withCategory("Non-existent category"),
+                new SearchCatalogRequest().withCategory("Another non-existent category")
+        );
     }
 }
