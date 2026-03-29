65f571b36d052d2d04bf5230bbdb927a77e31dcf
w.alexander <will_alexander@me.com>
2026-03-29 22:54:09 +0200

    Refactor ProductCategory to a record

--- catalog/src/main/java/tech/reactiv/ecommerce/catalog/product/ProductCategory.java
+++ catalog/src/main/java/tech/reactiv/ecommerce/catalog/product/ProductCategory.java
@@ -1,32 +1,9 @@
 package tech.reactiv.ecommerce.catalog.product;
 
-public class ProductCategory {
-
-    private final String category;
-
-    public ProductCategory(String category) {
-        if (category == null || category.isBlank()) {
+public record ProductCategory(String value) {
+    public ProductCategory {
+        if (value == null || value.isBlank()) {
             throw new IllegalArgumentException("Category must not be blank");
         }
-        this.category = category;
-    }
-
-    public String value() {
-        return this.category;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return obj instanceof ProductCategory other && category.equals(other.category);
-    }
-
-    @Override
-    public int hashCode() {
-        return category.hashCode();
-    }
-
-    @Override
-    public String toString() {
-        return category;
     }
 }
