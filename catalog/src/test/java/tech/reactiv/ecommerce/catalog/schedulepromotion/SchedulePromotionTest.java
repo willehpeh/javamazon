2b6542111f7a0346f3be195d5cee6f7d8b829913
w.alexander <will_alexander@me.com>
2026-03-30 14:45:39 +0200

    Parametrize test with PromotionTarget

--- catalog/src/test/java/tech/reactiv/ecommerce/catalog/schedulepromotion/SchedulePromotionTest.java
+++ catalog/src/test/java/tech/reactiv/ecommerce/catalog/schedulepromotion/SchedulePromotionTest.java
@@ -1,9 +1,11 @@
 package tech.reactiv.ecommerce.catalog.schedulepromotion;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import tech.reactiv.ecommerce.catalog.promotion.*;
 
 import java.time.LocalDate;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,14 +14,18 @@ public class SchedulePromotionTest {
     InMemoryPromotions promotions = new InMemoryPromotions();
     SchedulePromotionHandler handler = new SchedulePromotionHandler(promotions);
 
-    @Test
-    void shouldScheduleAllProductsPromotion() {
+    public static Stream<PromotionTarget> promotionTargets() {
+        return Stream.of(new AllProducts());
+    }
+
+    @ParameterizedTest
+    @MethodSource("promotionTargets")
+    void shouldSchedulePromotionsWithTarget(PromotionTarget target) {
         var id = PromotionId.create();
         var description = new PromotionDescription("10% off on all items");
         var discountPercent = new PromotionDiscountPercent(10);
         var startDate = LocalDate.now().plusDays(1);
         var endDate = startDate.plusDays(7);
-        var target = new AllProducts();
         var command = new SchedulePromotionCommand(id, description, discountPercent, startDate, endDate, target);
 
         handler.handle(command);
@@ -31,6 +37,6 @@ public class SchedulePromotionTest {
         assertThat(newPromotion.state().discountPercent()).isEqualTo(discountPercent.value());
         assertThat(newPromotion.state().startDate()).isEqualTo(startDate);
         assertThat(newPromotion.state().endDate()).isEqualTo(endDate);
-        assertThat(newPromotion.state().target()).isEqualTo(new AllProducts());
+        assertThat(newPromotion.state().target()).isEqualTo(target);
     }
 }
