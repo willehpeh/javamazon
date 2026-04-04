8cb53402c340f6076fe9496538c36b29aeeac700
w.alexander <will_alexander@me.com>
2026-04-04 15:17:30 +0200

    Add ProductAlreadyExistsException to API exception handler

--- catalog/src/main/java/tech/reactiv/ecommerce/catalog/api/CatalogExceptionHandler.java
+++ catalog/src/main/java/tech/reactiv/ecommerce/catalog/api/CatalogExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import tech.reactiv.ecommerce.catalog.product.ProductAlreadyExistsException;
 import tech.reactiv.ecommerce.catalog.product.ProductNotFoundException;
 
 import java.util.Map;
@@ -13,5 +14,9 @@ class CatalogExceptionHandler {
     @ExceptionHandler(ProductNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleProductNotFound(ProductNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("message", ex.getMessage()));
+    }
+    @ExceptionHandler(ProductAlreadyExistsException.class)
+    public ResponseEntity<Map<String, String>> handleProductAlreadyExists(ProductAlreadyExistsException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("message", ex.getMessage()));
     }
 }
