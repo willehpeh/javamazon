a1251f04e94d04045133ec533cdc120e06985c09
w.alexander <will_alexander@me.com>
2026-04-07 13:16:00 +0200

    Have CategoryId persist as flat UUID

--- catalog/src/main/java/tech/reactiv/ecommerce/catalog/category/CategoryId.java
+++ catalog/src/main/java/tech/reactiv/ecommerce/catalog/category/CategoryId.java
@@ -1,9 +1,11 @@
 package tech.reactiv.ecommerce.catalog.category;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 import java.util.Objects;
 import java.util.UUID;
 
-public record CategoryId(UUID value) {
+public record CategoryId(@JsonValue UUID value) {
     public CategoryId {
         Objects.requireNonNull(value, "Category ID cannot be null");
     }
