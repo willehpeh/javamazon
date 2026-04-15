f983f2337b6829dcd38e64cbb5e437b9c931f4a5
w.alexander <will_alexander@me.com>
2026-04-15 22:04:57 +0200

    Tighten acceptance test package scan

--- catalog/src/acceptanceTest/java/tech/reactiv/ecommerce/catalog/CatalogTestApplication.java
+++ catalog/src/acceptanceTest/java/tech/reactiv/ecommerce/catalog/CatalogTestApplication.java
@@ -2,6 +2,10 @@ package tech.reactiv.ecommerce.catalog;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "tech.reactiv.ecommerce")
+@SpringBootApplication(scanBasePackages = {
+        "tech.reactiv.ecommerce.catalog",
+        "tech.reactiv.ecommerce.common",
+        "tech.reactiv.ecommerce.shared"
+})
 class CatalogTestApplication {
 }
